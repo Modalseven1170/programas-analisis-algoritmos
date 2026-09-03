6d17022b05cbf16c765b69d5e5b5a66734bfe37f
Modalseven1170 <bruno.salazar1971@alumnos.udg.mx>
2026-09-03 08:49:31 -0600

    arreglo practicas

--- practica.1.py
+++ practica.1.py
@@ -1,5 +1,5 @@
-import time as t
-
+import time 
+tiempos_bubble_sort=[]
 def bubble_sort(arr):
     n = len(arr)
 
@@ -9,9 +9,16 @@ def bubble_sort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
-array = [6,5,3,1,8,7,2,4]
+#numeros = [147, 227, 234, 168, 967, 872, 259, 605, 686, 812, 60, 535, 256, 286, 718, 124, 757, 64, 71, 288, 774, 648, 524, 229, 893, 75, 779, 509, 409, 21]
+numeros = [147, 227, 234, 168, 967, 872, 259, 605, 686, 812, 60, 535, 256, 286, 718, 124, 757, 64, 71, 288, 774, 648, 524, 229, 893, 75, 779, 509, 409, 21, 147, 227, 234, 168, 967, 872, 259, 605, 686, 812, 60, 535, 256, 286, 718, 124, 757, 64, 71, 288, 774, 648, 524, 229, 893, 75, 779, 509, 409, 21]
 
-bubble_sort(array)
+Tini_ssort=time.time()
+print("Lista ordenada:", bubble_sort(numeros))
+Tfin_ssort=time.time()
+tiempos_bubble_sort.append(Tfin_ssort - Tini_ssort)
+print("Tiempos de ejecución (s):", tiempos_bubble_sort)
+
+bubble_sort(numeros)
 print("\n")
-print ("Lista ordenada:", array , "\n")
+print ("Lista ordenada:", numeros , "\n")
 print ("------------------------------")
--- randon.py
+++ randon.py
@@ -5,6 +5,6 @@ def genera(n, min, max):
     return [rd.randint(min, max) for i in range(n)]
 
 for i in N:
-    listas.append(genera(i, 1, 1000))
+    listas.append(genera(i, 1, 100))
 
 print (listas)
